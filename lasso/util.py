ec0dabf89c8b33a0bb92dcdfc8c8fa492640b0d2
Elizabeth Sall <easall@gmail.com>
2019-10-25 10:08:13 -0700

    fixed hash function to include osm node id

--- lasso/util.py
+++ lasso/util.py
@@ -1,20 +1,16 @@
-def get_shared_streets_intersection_hash(lat, long):
+def get_shared_streets_intersection_hash(lat, long, osm_node_id=None):
     """
     Calculated per:
        https://github.com/sharedstreets/sharedstreets-js/blob/0e6d7de0aee2e9ae3b007d1e45284b06cc241d02/src/index.ts#L553-L565
     Expected in/out
-     "Intersection -74.00482177734375 40.741641998291016"
-     69f13f881649cb21ee3b359730790bb9
-
-    Currently in/outing
-     'Intersection -74.00482177734375 40.741641998291016'
-     2a4a9e4ad1923f11ec46224f834d69a2
-
-     Tested float precisions of various levels...no luck
+      -93.0965985, 44.952112199999995 osm_node_id = 954734870
+       69f13f881649cb21ee3b359730790bb9
 
     """
     import hashlib
-
-    unhashed = "Intersection {0:.14f} {0:.15f}".format(long, lat).encode("utf-8")
-    hash = hashlib.md5(unhashed).encode("ascii").hexdigest()
+    message = "Intersection {0:.5f} {0:.5f}".format(long, lat)
+    if osm_node_id:
+        message += " {}".format(osm_node_id)
+    unhashed = message.encode("utf-8")
+    hash = hashlib.md5(unhashed).hexdigest()
     return hash
