6c87de229da486acf03c619262f37d244bd4735f
Sijia Wang <wangsijia0628@gmail.com>
2021-09-27 09:45:48 -0400

    mute error msg when bus_only is not in the network
    https://app.asana.com/0/0/1200166118391606/1200976105170228/f

--- lasso/mtc.py
+++ lasso/mtc.py
@@ -642,8 +642,9 @@ def calculate_cntype(
             return "MAZ"
         if x.roadway == "tap":
             return "TAP"
-        if x.bus_only == 1:
-            return "TANA"
+        if 'bus_only' in roadway_network.links_df.columns:
+            if x.bus_only == 1:
+                return "TANA"
         if x.drive_access == 1:
             return "TANA"
         elif x.walk_access == 1:
